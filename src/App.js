1cf02c2aca1e1a316b7eb2891ec2bbf50ff5410f
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-07 18:41:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ReactGA from 'react-ga';
+import { inject } from "@vercel/analytics/react"
 
 ReactGA.initialize('G-HPG4WBW6VK');
 
 function App() {
   return (
     <>
+      inject();
       <Routes>
         <Route path="/" element={<ProtectedRoutes> <HomePage /> </ProtectedRoutes>} />
         <Route path="/register" element={<Register />} />
