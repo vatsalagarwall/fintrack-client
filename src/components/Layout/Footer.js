ca8de257b4db75a6c391f9226d0b1aea50d88911
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-07 19:57:52 +0530

    commit

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='bg-dark text-light p-4'>
-            <h6 className='text-center'>Made by Vatsal</h6>
+            <h6 className='text-center'>Made by <a href="https://portfolio-vatsal.vercel.app/" target="_blank">Vatsal</a></h6>
         </div>
     )
 }
