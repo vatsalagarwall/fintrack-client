a3915b02425c67aaac8fcb35b84ac1d1456ede62
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-08 21:53:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import HomePage from "./pages/HomePage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
